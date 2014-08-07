7c44567b985f88c65d306821192b79d9eb6ba1db
Juan Pablo <ptorre@creativaweb.mx>
2014-08-07 13:50:20 -0500

    Ajustes finales
    Ajustes finales

--- 07-01/js/main.js
+++ 07-01/js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $('.lugares a').on('touchstart touchend', function() {
         $(this).toggleClass('activo');
     });
+
+	if ($(".bxslider").length > 0){
+	  $('.bxslider').bxSlider();
+	}
+
+	
+	$('#videodestacado').fitVids();
+    
 });
